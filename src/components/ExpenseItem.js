07bc3c5856896b098c895a62610204d72ad234aa
Evolusin <evoarcadias@gmail.com>
2022-09-07 15:50:02 +0200

    ExpanseDate done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expanseDate = new Date(2021, 2 ,25);
-  const expanseTitle = "Ubezpieczenie";
-  const expenseAmmount = 295.22;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expanseDate.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expanseTitle}</h2>
-        <div className="expense-item__price">{expenseAmmount} zł</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount} zł</div>
       </div>
     </div>
   );
