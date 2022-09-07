495a39443ddcc24e406fc0b6ffd639e72e0ce3b0
Evolusin <evoarcadias@gmail.com>
2022-09-07 11:58:01 +0200

    Css -> ExpemseItem.css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expanse item!</h2>
+  return (
+    <div>
+      <div>23 Sierpnia 2022</div>
+      <div>
+        <h2>Ubezpiecznie</h2>
+        <div>245 zł</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
-
